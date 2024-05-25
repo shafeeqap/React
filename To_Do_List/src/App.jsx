ecfa5ec513b85371dca893a97788bf666dbdfeb2
shafeeq <shafeeqap1986@gmail.com>
2024-05-26 01:54:52 +0530

    to do list

--- To_Do_List/src/App.jsx
+++ To_Do_List/src/App.jsx
@@ -1,12 +1,14 @@
 import ToDoList from "./ToDoList.jsx"
-
-
-
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   
   return (
-    <ToDoList />
+    <>
+      <ToDoList />
+      <ToastContainer/>
+    </>
   )
 }
 
--- To_Do_List/src/ToDoList.jsx
+++ To_Do_List/src/ToDoList.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from "react"
-
+import { toast } from 'react-toastify';
 
 
 function ToDoList(){
 
-    const [tasks, setTask] = useState(["Breakfast", "Take a shower", "Go to office"]);
+    const [tasks, setTask] = useState([]);
     const [newTask, setNewTask] = useState("");
+    const [editIndex, setEditIndex] = useState(null);
 
     const handleInputChange = (event) =>{
         setNewTask(event.target.value);
@@ -14,15 +15,35 @@ function ToDoList(){
 
     const addTask = () =>{
         if(newTask.trim() !==""){
-            setTask(tasks => [...tasks, newTask]);
+            if(!tasks.includes(newTask)){
+                if(editIndex !==null){
+                    const updatedTasks = [...tasks];
+                    updatedTasks[editIndex] = newTask;
+                    setTask(updatedTasks);
+                    setEditIndex(null)
+                    toast.success('Task edit successfully')
+                }else{
+                    setTask([...tasks, newTask]);
+                    toast.success('Task added successfully')
+                }
+            }else{
+                toast.warn("Task already exists. Please enter a different task.");
+            }
             setNewTask("")
+        }else{
+            toast.error('Please enter a vlid task!')
         }
-        
     };
+
+    const editTask = (index) =>{
+        setNewTask(tasks[index]);
+        setEditIndex(index);
+    }
 
     const deleteTask = (index) =>{
         const updatedTask = tasks.filter((_, i) => i !== index);
         setTask(updatedTask)
+        toast.error('Your task is deleted from the list.')
     };
 
     const moveUp = (index) =>{
@@ -49,17 +70,18 @@ function ToDoList(){
         <div className="content">
 
         <div style={{alignItems:"center"}}>
-            <input type="text" onChange={handleInputChange} placeholder="Enter a task..." value={newTask}></input>
-            <button className="add-button" onClick={addTask}>Add</button>
+            <input className="task-input" type="text" onChange={handleInputChange} placeholder="Enter a task..." value={newTask}></input>
+            <button className="add-button" onClick={addTask}>{editIndex !== null ? 'Update' : 'Add'}</button>
         </div>
     
         <ol>
             {tasks.map((task, index) =>
                 <li key={index} >
-                    <span className="text">{task}</span>
+                    <span className="task-text">{task}</span>
                     <button className="delete-button" onClick={() => deleteTask(index)}>🗑️</button>
                     <button className="moveUp" onClick={() => moveUp(index)}>⬆️</button>
                     <button className="moveDown" onClick={() => moveDown(index)}>⬇️</button>
+                    <button className="edit-button" onClick={() =>editTask(index)}>✏️</button>
                 </li>
             )}
         </ol>
