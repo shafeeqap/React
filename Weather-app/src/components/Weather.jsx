789de64b273462531dd9b347dece88d91e2ed60c
shafeeq <shafeeqap1986@gmail.com>
2024-09-29 19:51:33 +0530

    update

--- Weather-app/src/components/Weather.jsx
+++ Weather-app/src/components/Weather.jsx
@@ -1,106 +1,142 @@
 import "./Weather.css";
-import search_icon from "../assets/search.png"
-import clear_icon from "../assets/clear.png"
-import cloud_icon from "../assets/cloud.png"
-import drizzle_icon from "../assets/drizzle.png"
-import humidity_icon from "../assets/humidity.png"
-import rain_icon from "../assets/rain.png"
-import snow_icon from "../assets/snow.png"
-import wind_icon from "../assets/wind.png"
+import search_icon from "../assets/search.png";
+import humidity_icon from "../assets/humidity.png";
+import wind_icon from "../assets/wind.png";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useRef } from "react";
-
-
-
+import { Oval } from "react-loader-spinner";
 
 const Weather = () => {
-    const [weatherData, setWeatherData] = useState(false);
-    const inputRef = useRef();
+  const [weatherData, setWeatherData] = useState({
+    loading: false,
+    error: false,
+  });
+  const inputRef = useRef();
 
-    const allIcons = {
-        "01d": clear_icon,
-        "01n": clear_icon,
-        "02d": cloud_icon,
-        "02n": cloud_icon,
-        "03d": cloud_icon,
-        "03n": cloud_icon,
-        "04d": drizzle_icon,
-        "04n": drizzle_icon,
-        "09d": rain_icon,
-        "09n": rain_icon,
-        "10d": rain_icon,
-        "10n": rain_icon,
-        "13d": snow_icon,
-        "13n": snow_icon,
+  const toDate = () => {
+    const months = [
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    const currentDate = new Date();
+    const date = `${daysOfWeek[currentDate.getDay()]} 
+    ${currentDate.getDate()} ${months[currentDate.getMonth()]} 
+    ${currentDate.getFullYear()}`;
+    return date;
+  };
+
+  const search = async (city) => {
+    if (city === "") {
+      alert("Enter city name");
+      return;
     }
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
 
-    const search = async (city) => {
-        if(city === ""){
-            alert("Enter city name");
-            return;
-        }
-        try {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`
+      const response = await fetch(url);
+      const data = await response.json();
 
-            const response = await fetch(url);
-            const data = await response.json();
+      if (!response.ok) {
+        alert(data.message);
+        return;
+      }
 
-            if(!response.ok){
-                alert(data.message);
-                return;
-            }
-            
-            console.log(data);
-            const icon = allIcons[data.weather[0].icon] || clear_icon;
-            setWeatherData({
-                humidity: data.main.humidity,
-                windSpeed: data.wind.speed,
-                temperature: Math.floor(data.main.temp),
-                location: data.name,
-                icon: icon,
-            })
-            
-        } catch (error) {
-            setWeatherData(false);
-            console.log("Error in fetching weather data.", error);
-        }
+      console.log(data, "data");
+
+      const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      setWeatherData({
+        loading: true,
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        description: data.weather[0].description,
+        country: data.sys.country,
+        icon: icon,
+        time: data.timezone,
+      });
+    } catch (error) {
+      setWeatherData(false);
+      console.log("Error in fetching weather data.", error);
     }
+  };
 
-    useEffect(() =>{
-        search("london")
-    },[]);
-
+  useEffect(() => {
+    search("london");
+  }, []);
 
   return (
     <div className="weather">
-        <div className="search-bar">
-            <input ref={inputRef} type="text" placeholder="Search..."/>
-            <img src={search_icon} alt="" onClick={() => search(inputRef.current.value)}/>
-        </div>
-        {weatherData ? 
+      <div className="search-bar">
+        <input ref={inputRef} type="text" placeholder="Search..." />
+        <img
+          src={search_icon}
+          alt=""
+          onClick={() => search(inputRef.current.value)}
+        />
+      </div>
+      {weatherData ? (
         <>
-        <img src={weatherData.icon} alt="" className="weather-icon"/>
-        <p className="temperature">{weatherData.temperature}°C</p>
-        <p className="location">{weatherData.location}</p>
-        <div className="weather-data">
+          <div className="weather-container">
+            <img src={weatherData.icon} alt="" className="weather-icon" />
+            <div>
+              <p className="temperature">
+                {weatherData.temperature}
+                <span>°C</span>
+              </p>
+              <p className="description">{weatherData.description}</p>
+            </div>
+          </div>
+          <p className="location">
+            {weatherData.location} <span>{weatherData.country}</span>{" "}
+          </p>
+          <p className="date">{toDate()}</p>
+          <div className="weather-data">
             <div className="col">
-                <img src={humidity_icon} alt="" />
-                <div>
-                    <p>{weatherData.humidity} %</p>
-                    <span>Humidity</span>
-                </div>
+              <img src={humidity_icon} alt="" />
+              <div>
+                <p>{weatherData.humidity} %</p>
+                <span>Humidity</span>
+              </div>
             </div>
             <div className="col">
-                <img src={wind_icon} alt="" />
-                <div>
-                    <p>{weatherData.windSpeed} Km/h</p>
-                    <span>Wind Speed</span>
-                </div>
+              <img src={wind_icon} alt="" />
+              <div>
+                <p>{weatherData.windSpeed} Km/h</p>
+                <span>Wind Speed</span>
+              </div>
             </div>
-        </div>
-
-        </> : <></>}
+          </div>
+          {/* {weatherData.loading && (
+            <Oval type="Oval" color="green" height={70} width={70}></Oval>
+          )} */}
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
